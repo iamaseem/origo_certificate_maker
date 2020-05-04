d800717414ffa719f1d4a54823d1fae2c2c9e424
Aseem Muhammad A. R <asaseemas@gmail.com>
2020-05-04 14:22:34 +0530

    print_the_value

---
+++ main.py
@@ -0,0 +1,7 @@
+import jinja2
+
+templateLoader = jinja2.FileSystemLoader(searchpath="./")
+templateEnv = jinja2.Environment(loader=templateLoader)
+TEMPLATE_FILE = "name.txt"
+template = templateEnv.get_template(TEMPLATE_FILE)
+outputText = template.render(name="Aseem")
